56463bbb7951363cf7835c302cc023ffa468f213
ivankahnybediuk <56602081+ivankahnybediuk@users.noreply.github.com>
2020-12-23 15:19:53 +0300

    /

--- task4.py
+++ task4.py
@@ -5,3 +5,11 @@ The math quiz program
 
 Write a program that asks the answer for a mathematical expression, checks whether the user is right or wrong, and then responds with a message accordingly.
 """
+import random
+number1 = random.randint(1, 20)
+number2 = random.randint(1, 20)
+result = input(f'{number1} + {number2} = ' )
+if int(result) == number1+number2:
+    print('Yes! You`re right!')
+else:
+    print('No! It`s incorect!')
